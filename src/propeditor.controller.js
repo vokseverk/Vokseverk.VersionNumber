1d35b7df211e1b9b56844de8cfa9fb72119b9f54
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-02-27 17:55:08 +0100

    Remove config for minor

--- src/propeditor.controller.js
+++ src/propeditor.controller.js
@@ -1,16 +1,15 @@
 angular.module("umbraco").controller("VersionEditorController", function($scope, $element) {
 	if ($scope.model.config == null) {
 		$scope.model.config = {
-			useMinor: true,
-			usePatch: true
+			usePatch: 1
 		}
 	}
 	
-	if ($scope.model.value == null) {
+	if ($scope.model.value == "" || $scope.model.value == null) {
 		$scope.model.value = {
 			major: 1,
-			minor: $scope.model.config.useMinor ? 0 : null,
-			patch: ($scope.model.config.useMinor && $scope.model.config.usePatch) ? 0 : null
+			minor: 0,
+			patch: $scope.model.config.usePatch ? 0 : null
 		}
 	}
 })
