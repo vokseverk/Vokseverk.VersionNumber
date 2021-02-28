73797263511d4071def65c443b245c5dbd67cb35
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-02-28 12:58:05 +0100

    Try handling null

--- src/propeditor.controller.js
+++ src/propeditor.controller.js
@@ -1,15 +1,15 @@
 angular.module("umbraco").controller("VersionEditorController", function($scope, $element) {
-	if ($scope.model.config == null) {
+	if (!$scope.model.config) {
 		$scope.model.config = {
 			usePatch: 1
 		}
 	}
 	
-	if ($scope.model.value == "" || $scope.model.value == null) {
+	if (!$scope.model.value) {
 		$scope.model.value = {
 			major: 1,
 			minor: 0,
-			patch: $scope.model.config.usePatch ? 0 : null
+			patch: ($scope.model.config.usePatch == 1) ? 0 : null
 		}
 	}
 })
