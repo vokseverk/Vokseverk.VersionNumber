60a6dea1d7d483dc416796845a8fbdbcd7be5684
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-03-01 13:47:50 +0100

    Handle existing data
    Try to parse an existing value that "looks" like a version number, when replacing e.g. a textstring property

--- src/propeditor.controller.js
+++ src/propeditor.controller.js
@@ -11,5 +11,20 @@ angular.module("umbraco").controller("VersionEditorController", function($scope,
 			minor: 0,
 			patch: ($scope.model.config.usePatch == 1) ? 0 : null
 		}
+	} else if (typeof($scope.model.value) == "string") {
+		var value = $scope.model.value
+		var usePatch = $scope.model.config.usePatch == 1
+		var versionRE = /^\s*v?(\d+)\.(\d+)(?:\.(\d+))?\s*$/;
+		var matches = versionRE.exec(value)
+		if (matches != null && matches.length > 1) {
+			var maj = matches[1]
+			var min = matches[2] || 0
+			var pat = usePatch ? (matches[3] || 0) : null
+			$scope.model.value = {
+				major: maj,
+				minor: min,
+				patch: pat
+			}
+		}
 	}
 })
